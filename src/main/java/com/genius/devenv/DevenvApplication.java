15943ceb7081a9f9ff4ed21e0af3681dd92e0f3e
geniuschung <43457396+geniuschung@users.noreply.github.com>
2020-07-19 00:27:20 +0900

    init

---
+++ src/main/java/com/genius/devenv/DevenvApplication.java
@@ -0,0 +1,13 @@
+package com.genius.devenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevenvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevenvApplication.class, args);
+    }
+
+}
